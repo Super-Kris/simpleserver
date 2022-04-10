b87a706964d1d95127108a915ca0f16882494370
Super-Kris <72203587+Super-Kris@users.noreply.github.com>
2022-04-10 09:46:46 +0200

    zaktualizowany response

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Kris jest super SUPER!');
+  res.end('Kris jest SUPER!');
 }
 
 const server = http.createServer(requestListener);
